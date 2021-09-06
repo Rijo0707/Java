bc70beebf4e5dd92401555eb8f55b2e64eed0aaf
Rijo0707 <77331943+Rijo0707@users.noreply.github.com>
2021-09-06 18:57:35 +0530

    created helloworld.java
    Program to print hello world

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author Rijo Saju Verghese
+ */
+public class helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
